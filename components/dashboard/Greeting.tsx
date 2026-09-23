98a4f3e0dc2848767061baa66a9d8705da3688b6
י.מ. מאואס <ysrlmyrmws1@gmail.com>
2026-09-24 02:17:05 +0300

    Delete components/dashboard/Greeting.tsx
    רק הרס

--- components/dashboard/Greeting.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useLocale } from "@/lib/i18n/locale-provider";
-
-export function Greeting({ name }: { name: string }) {
-  const { t } = useLocale();
-  return <>{t("dash.greeting").replace("{name}", name)}</>;
-}
